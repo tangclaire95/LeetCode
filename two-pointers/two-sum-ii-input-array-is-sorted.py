e2508439a6c08c266c159556cd8b23b3d2f0fd83
Claire Tang <97918087+tangclaire95@users.noreply.github.com>
2025-12-16 18:53:11 -0500

    LC-two-sum-ii-input-array-is-sorted [Runtime: 2 ms; Memory: 13 MB]

--- two-pointers/two-sum-ii-input-array-is-sorted.py
+++ two-pointers/two-sum-ii-input-array-is-sorted.py
@@ -1,18 +1,12 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left= 0
-        right= len(numbers)-1
-        while True: #left<right
-            s=numbers[left]+numbers[right]
-            if s== target:
-                break
-            if s>target:
-                right -=1
+class Solution(object):
+    def twoSum(self, numbers, target):
+        left, right=0, len(numbers) -1
+        while left<right: 
+            current_sum=numbers[left] +numbers[right]
+            if current_sum==target:
+                return [left+1, right+1]
+            elif current_sum < target:
+                left += 1
             else: 
-                left +=1
-        return [left+1, right+1]
-    
-
-    # time o(n)
-    # space o(1)
-    
+                right -=1
+        return [] 
